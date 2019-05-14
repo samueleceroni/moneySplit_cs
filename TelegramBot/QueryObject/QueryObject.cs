84cea6af1e9baa0136a6f065e0ef4aa9533889d8
Filippo Sanzani <filippo.sanzani2001@gmail.com>
2019-05-14 13:01:01 +0200

    ChatId & ChatMember added in QueryObject

--- TelegramBot/QueryObject/QueryObject.cs
+++ TelegramBot/QueryObject/QueryObject.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using CSharpFunctionalExtensions;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot.Types;
 
 namespace TelegramBot.QueryObject
 {
@@ -14,18 +15,24 @@ namespace TelegramBot.QueryObject
         public double Value { get; private set; }
         public string Description { get; private set; }
         public List<string> Tags { get; private set; }
+        public ChatId Chat { get; private set; }
+        public ChatMember ChatMember { get; private set; }
 
-        private QueryObject(QueryType type, string listName, double value, string description, List<string> tags)
+        private QueryObject(ChatId chat, ChatMember chatmember, QueryType type, string listName, double value, string description, List<string> tags)
         {
             Type = type;
             ListName = listName;
             Value = value;
             Description = description;
             Tags = tags;
+            Chat = chat;
+            ChatMember = chatmember;
         }
 
-        public static Result<QueryObject> BuildQueryObject(QueryType type, string listName, double value, string description, List<string> tags)
+        public static Result<QueryObject> BuildQueryObject(ChatId chat, ChatMember chatMember, QueryType type, string listName, double value, string description, List<string> tags)
         {
+            if (chat == null)
+                return Result.Fail<QueryObject>("Chat is null");
             if (type == QueryType.None)
                 return Result.Fail<QueryObject>("Type can't be set to None");
             if (string.IsNullOrEmpty(listName))
@@ -33,6 +40,8 @@ namespace TelegramBot.QueryObject
 
             return Result.Ok<QueryObject>(
                     new QueryObject(
+                            chat, 
+                            chatMember,
                             type,
                             listName, 
                             value, 
--- TelegramBot/QueryObject/QueryObjectBuilder.cs
+++ TelegramBot/QueryObject/QueryObjectBuilder.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using CSharpFunctionalExtensions;
 using System.Threading.Tasks;
+using Telegram.Bot.Types;
 
 namespace TelegramBot.QueryObject
 {
@@ -14,16 +15,22 @@ namespace TelegramBot.QueryObject
         public double Value { get; set; }
         public string Description { get; set; }
         public List<string> Tags { get; set; }
+        public ChatId Chat { get; set; }
+        public ChatMember ChatMember { get; set; }
 
         public QueryObjectBuilder()
         {
             Tags = new List<string>();
             Type = QueryType.None;
+            Chat = null;
+            ChatMember = null;
         }
 
         public Result<QueryObject> Build()
         {
             return QueryObject.BuildQueryObject(
+                    Chat,
+                    ChatMember,
                     Type,
                     ListName,
                     Value,
